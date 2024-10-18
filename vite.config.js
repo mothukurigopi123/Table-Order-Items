687e903f564d620894c5811437172545991bc6e7
mothukurigopi123 <147327671+mothukurigopi123@users.noreply.github.com>
2024-10-19 01:40:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/<repository-name>/',
+    base: '/<Table-Order-Items>/',
   plugins: [react()],
 })
